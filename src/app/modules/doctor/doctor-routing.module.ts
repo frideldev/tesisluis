8b2b1b62b60e74e15fc39cf14ca91535147a366e
frideldev <fridel@gmail.com>
2022-03-23 21:15:41 -0400

    diseñologin

--- src/app/modules/doctor/doctor-routing.module.ts
+++ src/app/modules/doctor/doctor-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendarCitaComponent } from './pages/agendar-cita/agendar-cita.component';
+import { CitasComponent } from './pages/citas/citas.component';
+import { HistorialComponent } from './pages/historial/historial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  children:[
+    {path:'historial', component:HistorialComponent},
+    {path:'agendar-cita', component:AgendarCitaComponent},
+    {path:'citas', component:CitasComponent},
+  ]
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
